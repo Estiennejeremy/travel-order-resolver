6fc5ccc86e0e50b71b63ec878d4d6325dbe05c11
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2021-10-28 19:08:46 +0200

    feat: add chakra ui and record button

--- travel-order-resolver-front/src/App.js
+++ travel-order-resolver-front/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./App.css";
-
+import { Heading, Stack, Text } from "@chakra-ui/layout";
+import { IconButton } from "@chakra-ui/react";
+import { FaMicrophone } from "react-icons/fa";
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Stack py={4} direction="column" spacing={6} alignItems="center">
+      <Heading as="h1" color="whiteAlpha.500">
+        Travel Order Resolver
+      </Heading>
+      <Text color="whiteAlpha.500">
+        Click on the "record" button and ask to go to a destination by
+        specifying your starting point
+      </Text>
+      <IconButton
+        isRound={true}
+        icon={<FaMicrophone />}
+        size="lg"
+        colorScheme="red"
+      />
+    </Stack>
   );
 }
 
